6baf46458f1f621716a7d64c3ac352f6ca26a21e
Tom Booth <tombooth@gmail.com>
2017-08-11 14:50:14 +0100

    More specific error code processing
    Sometimes we want to ignore certain error codes, or maybe give a
    slightly clearer alert as to what went wrong.
    
    Given that processes get terminated with a SIGTERM when kubernetes wants
    to stop them, we are not interested in the JVM telling us that happened.

--- rules/unsuccessful_exit.go
+++ rules/unsuccessful_exit.go
@@ -3,7 +3,7 @@ package rules
 import (
 	"fmt"
 
-	log "github.com/Sirupsen/logrus"
+	//log "github.com/Sirupsen/logrus"
 
 	"k8s.io/api/core/v1"
 	"k8s.io/apimachinery/pkg/runtime"
@@ -16,16 +16,28 @@ var UnsuccessfulExitRule = NewRule(
 		pod := new.(*v1.Pod)
 
 		for _, c := range pod.Status.ContainerStatuses {
-			if c.State.Terminated != nil && c.State.Terminated.ExitCode > 0 {
-				log.Debugf("Unhappy pod %s", pod.ObjectMeta.Name)
-
-				out <- &alerts.Alert{
-					new,
-					fmt.Sprintf(
-						"Pod `%s.%s` (container: `%s`) has failed with exit code: `%d`",
-						pod.ObjectMeta.Namespace, pod.ObjectMeta.Name, c.Name, c.State.Terminated.ExitCode,
-					),
+			if c.State.Terminated != nil {
+				switch exitCode := c.State.Terminated.ExitCode; exitCode {
+				case 0:    // Everything was OK
+				case 143:  // JVM SIGTERM
+				case 137:  // Process got SIGKILLd
+					out <- &alerts.Alert{
+						new,
+						fmt.Sprintf(
+							"Pod `%s.%s` (container: `%s`) was killed by a SIGKILL. Please make sure you are shuting down in time, or extend `terminationGracePeriodSeconds` on your pod.",
+							pod.ObjectMeta.Namespace, pod.ObjectMeta.Name, c.Name, c.State.Terminated.ExitCode,
+						),
+					}
+				default:
+					out <- &alerts.Alert{
+						new,
+						fmt.Sprintf(
+							"Pod `%s.%s` (container: `%s`) has failed with exit code: `%d`",
+							pod.ObjectMeta.Namespace, pod.ObjectMeta.Name, c.Name, c.State.Terminated.ExitCode,
+						),
+					}
 				}
+
 			}
 		}
 	},
