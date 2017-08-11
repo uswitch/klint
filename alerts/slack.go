a3fd7e24d6607e8987d9d19ee198910dbb82b822
Tom Booth <tombooth@gmail.com>
2017-08-11 10:34:25 +0100

    Set username to message slack with
    Otherwise it just comes through as `bot` which is very boring

--- alerts/slack.go
+++ alerts/slack.go
@@ -6,12 +6,14 @@ import (
 )
 
 type SlackOutput struct {
-	client *slack.Client
+	client   *slack.Client
+	username string
 }
 
-func NewSlackOutput(token string) *SlackOutput {
+func NewSlackOutput(token, username string) *SlackOutput {
 	return &SlackOutput{
-		client: slack.New(token),
+		client:   slack.New(token),
+		username: username,
 	}
 }
 
@@ -19,7 +21,10 @@ func (s *SlackOutput) Key() string { return "slack" }
 
 func (s *SlackOutput) Send(val string, message string) error {
 	log.Debugf("SLACK: #%s %s", val, message)
-	_, _, err := s.client.PostMessage(val, message, slack.PostMessageParameters{})
+	_, _, err := s.client.PostMessage(val, message, slack.PostMessageParameters{
+		AsUser:   false,
+		Username: "klint",
+	})
 
 	return err
 }
