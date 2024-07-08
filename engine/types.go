14cbdb6f2aa84fba8ca2df90d6e48951a534eb03
Matteo Mori <matteo.mori@rvu.co.uk>
2024-07-08 10:29:03 +0100

    fix: Remove check on deprecated API for CronJobs

--- engine/types.go
+++ engine/types.go
@@ -4,7 +4,7 @@ import (
 	"fmt"
 
 	appsv1 "k8s.io/api/apps/v1"
-	batchv1 "k8s.io/api/batch/v1beta1"
+	batchv1 "k8s.io/api/batch/v1"
 	v1 "k8s.io/api/core/v1"
 	networkingv1 "k8s.io/api/networking/v1"
 	"k8s.io/apimachinery/pkg/runtime"
